c8457a7189c5247de15725da8d0d4370f957a8bd
OIgold-web <mastad1432@gmail.com>
2025-12-21 19:46:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ async function fetchImg(){
     if(searchBox.value == ''){
       keyword = "quotes";
     }else{
-      keyword = searchBox.value + "quotes";
+      keyword = searchBox.value;
     }
     const res = await fetch(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=RrqyvCmMDZjnSaL8EOIqEmli-IeXfQ1UiN8ot2_psDI&page=${page}&per_page=10`);
     
